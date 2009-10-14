2006e8e5bb6829d4058e391ebb90968c0e0bb826
Ross Andrews <randrews@geekfu.org>
2009-10-13 23:50:54 -0500

    Basic flow of actions in quiz controller

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -1,23 +1,65 @@
 class QuizController < ApplicationController
-  before_filter :find_quiz, :only=>:take_quiz
-  before_filter :init_quiz_data, :only=>:take_quiz
+=begin
+The general flow of actions looks like this:
+
+Take_quiz is a dispatcher. It sends you to either a
+title page, a question page or a results page
+depending on your session.
+
+Going to /foo takes you to the take_quiz for foo,
+which initially creates your session data and shows
+you a quiz title page.
+
+This has a "start quiz" link that sends you to
+/foo/start, which updates your current_question to
+the first question and sends you back to take_quiz.
+
+When you answer a question, it sends you to answer,
+which updates your session and sends you back to
+take_quiz.
+
+Eventually you reach the end of the questions, and
+take_quiz sends you to a quiz_results page.
+=end
+
+  before_filter :quiz
+  before_filter :quiz_data
 
   def take_quiz
+    puts session.inspect
     if @quiz_data[:current_question]==0
       render "take_quiz"
+    elsif @quiz_data[:current_question].nil?
+      render "quiz_results"
     else
+      @current_question = @quiz.questions.find{|q| q.id == @quiz_data[:current_question]}
       render "quiz_question"
     end
   end
 
+  def start
+    @quiz_data[:current_question] = @quiz.questions.first.id
+    redirect_to take_quiz_url(params[:short_name])
+  end
+
+  def answer
+    @quiz_data["#{params[:question_id]}_answer"] = params[:answer_id]
+
+    current_question_index = @quiz.questions.find{|q| q.id==params[:question_id]}
+    next_question_id = @quiz.questions[current_question_index].try :id
+    @quiz_data[:current_question] = next_question_id
+
+    redirect_to take_quiz_url(params[:short_name])
+  end
+
   private
 
-  def find_quiz
+  def quiz
     @quiz = Quiz.find_by_short_name params[:short_name]
     raise "Couldn't find quiz #{params[:short_name]}" unless @quiz
   end
 
-  def init_quiz_data
+  def quiz_data
     session[:quiz_data] ||= {}
     session[:quiz_data][@quiz.short_name] ||= @quiz.init_session_data
     @quiz_data = session[:quiz_data][@quiz.short_name]
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ ActionController::Routing::Routes.draw do |map|
   map.resource :session
 
   map.take_quiz "/quiz/:short_name", :controller=>"quiz", :action=>"take_quiz"
+  map.answer_quiz "/quiz/:short_name/:question_id/:answer_id", :controller=>"quiz", :action=>"answer"
+  map.start_quiz "/quiz/:short_name/start", :controller=>"quiz", :action=>"start"
 
   # Sample of regular route:
   #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
